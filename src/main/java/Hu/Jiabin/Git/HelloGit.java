dfefce29c87a2f17704bfbe92cf918dd039ef2e2
jiabinhu <1115806174@qq.com>
2021-08-13 23:34:18 +0800

    乱码解决

--- src/main/java/Hu/Jiabin/Git/HelloGit.java
+++ src/main/java/Hu/Jiabin/Git/HelloGit.java
@@ -5,21 +5,23 @@ public class HelloGit
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Git");
-		System.out.println("����1");
-		System.out.println("������֧1");//Merge�ϲ���֧   Branch������֧
-		System.out.println("�������");
-		System.out.println("������֧1");
-		System.out.println("��֧���");
-		//��github�������Ƚ�����һ�����߸��£�
-		//���µĽ�����û�и��µ����µİ汾��
-		//�����ύ����ʧ��
-		System.out.println("�ڶ����ύ֮ǰ�ĸ���");
-		//ʹ��pull,���±��زֿ�
-		System.out.println("�������ύ����");
-		System.out.println("���ߵĸ���");
+		System.out.println("更新1");
+		
+	
+		System.out.println("创建分支1");//Merge合并分支   Branch创建分支
+		System.out.println("主干添加");
+		System.out.println("创建分支1");
+		System.out.println("分支添加");
+		//在github里面首先进行了一个在线更新，
+		//导致的结果这边没有更新到最新的版本。
+		//重新提交更新失败
+		System.out.println("第二次提交之前的更新");
+		//使用pull,更新本地仓库
+		System.out.println("第三次提交之前的更新");
 		System.out.println("乱码解决");
+	}
 
 
 	
-	}
+	
 }
