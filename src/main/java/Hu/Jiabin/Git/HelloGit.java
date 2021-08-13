2cba8ea2d012a4bc169844b1b340d3cd81b000bf
jiabinhu <51167328+jiabinhu@users.noreply.github.com>
2021-08-13 23:35:21 +0800

    pull乱码解决

--- src/main/java/Hu/Jiabin/Git/HelloGit.java
+++ src/main/java/Hu/Jiabin/Git/HelloGit.java
@@ -5,19 +5,7 @@ public class HelloGit
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Git");
-		System.out.println("����1");
-		System.out.println("������֧1");//Merge�ϲ���֧   Branch������֧
-		System.out.println("�������");
-		System.out.println("������֧1");
-		System.out.println("��֧���");
-		//��github�������Ƚ�����һ�����߸��£�
-		//���µĽ�����û�и��µ����µİ汾��
-		//�����ύ����ʧ��
-		System.out.println("�ڶ����ύ֮ǰ�ĸ���");
-		//ʹ��pull,���±��زֿ�
-		System.out.println("�������ύ����");
-		System.out.println("���ߵĸ���");
-		System.out.println("乱码解决");
+		System.out.println("乱码的解决");
 
 
 	
